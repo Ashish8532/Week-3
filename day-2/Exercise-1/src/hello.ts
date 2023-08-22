70c9c1c27c48524811209f14ae1a791c940bb3aa
Ashish8532 <ashishraj916263@outlook.com>
2023-08-22 16:13:35 +0530

    add function greet to print message

---
+++ day-2/Exercise-1/src/hello.ts
@@ -0,0 +1,10 @@
+// greet function take string name as parameter
+// return Message with Hello name.
+function greet(name: string): string
+{
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("Ashish"));
+console.log(greet("Abhiraj"));
+console.log(greet("Vipul"));
