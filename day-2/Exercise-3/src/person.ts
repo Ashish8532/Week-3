2cbd0f228a0b642cdf9496be1c7d26dd795bf8e6
Ashish8532 <ashishraj916263@outlook.com>
2023-08-22 17:05:34 +0530

    resolved the bug by debugging

--- day-2/Exercise-3/src/person.ts
+++ day-2/Exercise-3/src/person.ts
@@ -12,10 +12,7 @@ function introduce(person: Person)
     console.log(`${person.firstName} ${person.lastName} is ${person.age} years old`);
 }
 
-// Here I am passing lastname as number but it should be a string.
-// And It gives the compile time error that lastname is not assignable to 
-// parameter of type Person as the lastname type is string.
 
-let myObj = { firstName: "Ashish", lastName: 12};
-
+// Resolve all bug using VSCode debugger.
+let myObj = { firstName: "Ashish", lastName: "Kumar", age: 21 };
 introduce(myObj);
